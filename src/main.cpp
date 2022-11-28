e0a3a5771b23aa20dac0fd7af602a5e204aa9c67
tseng <s07151@ms1.hinet.net>
2022-11-28 16:17:08 +0800

    變更為控制GPIO2

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,29 @@
 #include <Arduino.h>
 #include <BluetoothSerial.h>
 BluetoothSerial BT;
-void setup() {
+char val;      // 宣告存放串列埠資料字元變數
+int led=2;             //宣告led 接於 GPIO 2
+void setup() 
+{
   Serial.begin(115200);
-  BT.begin("tsengBT");   //藍芽顯示名稱,請修改為自訂名稱  //修改第二次
+  BT.begin("tsengBT");   //藍芽顯示名稱,請修改為自訂名稱
+  pinMode(led , OUTPUT);     //設定 GPIO 2 為輸出 
 }
 
-void loop() {
-  
-  while (Serial.available()) {   //檢查序列內是否有資料
-    String Sdata = Serial.readString();   //讀取序列資料
-    BT.println(Sdata);   //傳輸給藍芽
+void loop() 
+{
+  if (BT.available())  //判斷藍芽串列埠上是否有資料
+  {
+    val=BT.read();   //讀取串列資料，存放於val
+    switch (val)
+    {
+      case '0':
+         digitalWrite(led,LOW);
+         break;
+      case '1':
+         digitalWrite(led,HIGH);
+         break;
+    }
+
   }
-  
-  while (BT.available()) {   //檢查藍芽內是否有資料
-    String BTdata = BT.readString();    //讀取藍芽資料
-    Serial.println(BTdata);   //顯示在序列視窗
-  }
-  delay(1);
 }
